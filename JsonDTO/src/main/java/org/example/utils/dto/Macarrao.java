bc238b945ce12ade03b553a02d5c94558a526670
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-05-08 14:34:53 -0300

    Add: Java JSON to DTO

---
+++ JsonDTO/src/main/java/org/example/utils/dto/Macarrao.java
@@ -0,0 +1,22 @@
+package org.example.utils.dto;
+
+public class Macarrao {
+    private String mc1;
+    private String mc2;
+
+    public String getMc1() {
+        return mc1;
+    }
+
+    public void setMc1(String mc1) {
+        this.mc1 = mc1;
+    }
+
+    public String getMc2() {
+        return mc2;
+    }
+
+    public void setMc2(String mc2) {
+        this.mc2 = mc2;
+    }
+}
---
+++ JsonDTO/src/main/java/org/example/utils/dto/Root.java
@@ -0,0 +1,7 @@
+package org.example.utils.dto;
+
+public class Root {
+//    @JsonProperty("Data")
+    public Data data;
+
+}
